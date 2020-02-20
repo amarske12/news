1c28eb1dd00e816fb25b56c361cde9873078e4e9
amarske12 <adammarske@gmail.com>
2020-02-20 05:46:01 +0000

    Wednesday night work

--- app.rb
+++ app.rb
@@ -7,12 +7,30 @@ def view(template); erb template.to_sym; end
 before { puts "Parameters: #{params}" }                                     
 
 # enter your Dark Sky API key here
-ForecastIO.api_key = "YOUR-API-KEY"
+ForecastIO.api_key = "d0d3daee56a2c8ee92e80850cd1d72b3"
 
 get "/" do
-  # show a view that asks for the location
+  view "ask"
 end
 
 get "/news" do
-  # do everything else
+    results = Geocoder.search(params["q"])
+    lat_long = results.first.coordinates # => array [lat, long]
+    
+    #send lat_long to the Dark Sky API. Retrieve the result forecasted weather in a user-friendly format.
+    forecast = ForecastIO.forecast(lat_long[0],lat_long[1])
+    
+    #display the current
+    @current_temperature = forecast["currently"]["temperature"]
+    @conditions = forecast["currently"]["summary"]
+    @location = forecast["timezone"]
+       
+    view "paper"
+    
 end
+        #forecasted weather in a user-friendly format.
+        #<ul> 
+         #   <%for days in @forecast["daily"]["data"]%>
+          #      <li><% "Expect a high temperature of #{days["temperatureHigh"]}, low of #{days["temeratureLow"]} and #{days["summary"]} conditions"%></li>
+           # <%end%>
+        #</ul>
